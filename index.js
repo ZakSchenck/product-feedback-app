dff217c9265372cf3cdd53d7c3e0ad5dee912a39
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-04-10 17:11:28 -0400

    added port env

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.use('/api/v1/user', user)
 app.use('/api/v1/recommendation/', recommendation)
 
 
-app.listen(4000, console.log('we are running on port 4000'))
+app.listen(process.env.PORT || 4000, console.log('we are running on port 4000'))
